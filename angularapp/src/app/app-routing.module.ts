778e309e02d554075b86af37ec9736ec9d54753e
irfankhan27 <mohammadirfankhan2704@gmail.com>
2025-04-15 09:38:47 +0000

    hi

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { AdmineditinternshipComponent } from './components/admineditinternship/admineditinternship.component';
 import { LoginComponent } from './components/login/login.component';
+import { ErrorComponent } from './components/error/error.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {path:'registration',component:RegistrationComponent},
   {path:'admineditinternship',component:AdmineditinternshipComponent},
   {path:'login', component:LoginComponent},
-  {path:'',redirectTo:'admineditinternship',pathMatch:'full'}
+  {path:'',redirectTo:'admineditinternship',pathMatch:'full'},
+  {path:'error',component:ErrorComponent},
+  {path:'home',component:HomeComponent},
+  {path:'adminineditinternship',component:AdmineditinternshipComponent}
 ];
 
 @NgModule({
