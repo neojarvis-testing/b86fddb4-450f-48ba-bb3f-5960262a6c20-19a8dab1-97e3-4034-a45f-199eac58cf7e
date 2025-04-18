32809cfa4b47539220fbd2d035ec1a2ea9ba4409
irfankhan27 <mohammadirfankhan2704@gmail.com>
2025-04-18 11:57:30 +0000

    ko

--- angularapp/src/app/components/internshippiechart/internshippiechart.component.ts
+++ angularapp/src/app/components/internshippiechart/internshippiechart.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 //import { ChartType, ChartData, ChartOptions } from 'chart.js';
+import { InternshipService } from 'src/app/services/internship.service';
+import { Chart } from 'chart.js';
 
-interface DegreeProgram {
-  name: string;
-  percentage: number;
-}
+// interface DegreeProgram {
+//   name: string;
+//   percentage: number;
+// }
 
 @Component({
   selector: 'app-internshippiechart',
@@ -38,4 +40,37 @@ export class InternshippiechartComponent {
 //       datasets: [{ data: this.programs.map(program => program.percentage) }]
 //     };
 //   }
+
+
+
+  constructor(private internshipService: InternshipService) {}
+
+  ngOnInit() {
+    this.internshipService.getAllInternshipApplications().subscribe(data => {
+      const degreePrograms = data.map(app => app.DegreeProgram);
+      const degreeCounts = degreePrograms.reduce((acc, program) => {
+        acc[program] = (acc[program] || 0) + 1;
+        return acc;
+      }, {});
+
+      const labels = Object.keys(degreeCounts);
+      const values = Object.values(degreeCounts);
+
+      new Chart('degreeChart', {
+        type: 'pie',
+        data: {
+          labels,
+          datasets: [{
+            data: values,
+            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF', '#FF9F40']
+          }]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false
+        }
+      });
+    });
+  }
+
 }
